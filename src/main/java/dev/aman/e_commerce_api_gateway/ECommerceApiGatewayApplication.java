f76e783cb667473ba0d664e135dbfaaaed2069ad
githubgitAman <sharma.aman.a30@gmail.com>
2025-04-09 22:54:33 +0530

    Implemented API Gateway

---
+++ src/main/java/dev/aman/e_commerce_api_gateway/ECommerceApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package dev.aman.e_commerce_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/aman/e_commerce_api_gateway/ECommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aman.e_commerce_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
